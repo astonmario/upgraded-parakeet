2003dfdebe19af1b165d2e0039b2829b06f50a69
方浩远 <fanghaoyuan>
2021-03-10 22:35:45 +0800

    照搬框架

--- password-manager/src/App.vue
+++ password-manager/src/App.vue
@@ -11,6 +11,9 @@ export default {
 </script>
 
 <style>
+    @import "./assets/css/main.css";
+    @import "./assets/css/color-dark.css";     /*深色主题*/
+
 head,
 html,
 body {
--- password-manager/src/router/index.js
+++ password-manager/src/router/index.js
@@ -10,6 +10,23 @@ export default new Router({
       path: '/',
       redirect: '/dashboard'
   },
+  {
+    path: '/',
+    component: () => import(/* webpackChunkName: "home" */ '../components/common/Home.vue'),
+    meta: { title: '自述文件' },
+    children: [
+        {
+            path: '/dashboard',
+            component: () => import(/* webpackChunkName: "dashboard" */ '../components/page/Dashboard.vue'),
+            meta: { title: '系统首页' }
+        },
+        {
+            path: '/table',
+            component: () => import(/* webpackChunkName: "table" */ '../components/page/BaseTable.vue'),
+            meta: { title: '基础表格' }
+        },
+    ]
+},
     {
       path: '/',
       name: 'Login',
